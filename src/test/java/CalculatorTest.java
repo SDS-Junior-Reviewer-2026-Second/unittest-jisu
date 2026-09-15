4ecac774d18d272d8512662660fafeb5b3c658f5
Jisu Hong <jaylovegood@snu.ac.kr>
2026-09-15 17:50:01 +0900

    completed assignment

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,13 +1,37 @@
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class CalculatorTest {
 
-    Calculator calculator = new Calculator();
+    @Mock
+    Adder adder;
+
+    Calculator calculator;
+
+    @BeforeEach
+    void setup(){
+
+        when(adder.add(Mockito.anyInt(), Mockito.anyInt()))
+                .thenAnswer(invocation -> {
+                    int a = invocation.getArgument(0);
+                    int b = invocation.getArgument(1);
+                    return a + b;
+                });
+
+        calculator = new Calculator(adder);
+
+    }
 
     @Test
     void add() {
@@ -15,7 +39,6 @@ class CalculatorTest {
         assertEquals(6, res);
     }
 
-    @Disabled
     @Test
     void subtract() {
         int res = calculator.subtract(1, 5);
@@ -36,6 +59,7 @@ class CalculatorTest {
         assertThrows(ArithmeticException.class, () -> calculator.divide(6, 0));
     }
 
+    @Disabled
     @Test
     void testAssertJ(){
         String abc = "1234567";
